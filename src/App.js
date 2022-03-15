14097a074736d35ad9de93f5a31126835d5d8d52
Richard Guan <rguan72@gmail.com>
2022-03-15 18:20:23 -0400

    Finish overall layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import { useState } from 'react';
 import './App.css';
+import profile from './ui_user_profile.svg'
 
 export default function App() {
   const [ showModal, setShowModal ] = useState(false)
   return (
     <>
       <div className='flex flex-row place-content-between'>
-        <div className='flex flex-col ml-14 mt-32'>
-          <h1 className='text-6xl font-bold'>
+        <div className='flex flex-col ml-24 mt-32'>
+          <h1 className='text-9xl font-bold'>
             Townhall
           </h1>
-          <button className='px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm mt-8' onClick={() => setShowModal(true)}>Watch a Tutorial</button>
+          <button className='self-start px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm mt-8' onClick={() => setShowModal(true)}>Watch a Tutorial</button>
           <h3 className='font-bold mt-44'>Whenever you're ready</h3>
-          <button className='text-xl mt-4 px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm' onClick={() => setShowModal(true)}>Start a Meeting Now</button>
-          <button className='text-xl mt-4 px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm' onClick={() => setShowModal(true)}>Schedule a Meeting Later</button>
+          <button className='self-start text-2xl mt-4 px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm' onClick={() => setShowModal(true)}>Start a Meeting Now</button>
+          <button className='self-start text-2xl mt-4 px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm' onClick={() => setShowModal(true)}>Schedule a Meeting Later</button>
         </div>
-        <div className='flex flex-col text-right mr-14'>
-          <h1 className='mt-8'>Profile</h1>
-          <h1 className='font-bold text-left mt-80'>Planned Meetings</h1>
+        <div className='flex flex-col text-right mr-24'>
+          <img src={profile} alt="Profile" className='w-10 self-end mt-8'/>
+          <h1 className='font-bold text-left mt-[60vh] mb-1'>Planned Meetings</h1>
           <PlannedMeetings />
         </div>
         <Modal showModal={showModal} setShowModal={setShowModal} />
@@ -40,7 +41,7 @@ function Modal({ showModal, setShowModal }) {
             </div>
           </div>
         </div>
-        <input className='sm:ml-4 border' type='text' placeholder='Email: '/>
+        <input className='sm:ml-4 border mt-2 rounded pl-2' type='text' placeholder='Email: '/>
       </div>
       <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
         <button type="button" onClick={() => setShowModal(false)} class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">Submit</button>
@@ -63,9 +64,9 @@ function PlannedMeetings() {
         </div>
       </div>
       <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row border">
-        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Invite More Guests</button>
-        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Edit</button>
-        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+        <button type="button" class="text-sm mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Invite More Guests</button>
+        <button type="button" class="text-sm mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Edit</button>
+        <button type="button" class="text-sm mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
       </div>
     </>
   )
