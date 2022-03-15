f77d6abdbfc774cf0b4b82e8cb1b15e8734163dd
Richard Guan <rguan72@gmail.com>
2022-03-15 17:59:18 -0400

    Get some basic stuff going for townhall

--- src/App.js
+++ src/App.js
@@ -1,9 +1,79 @@
+import { useState } from 'react';
 import './App.css';
 
 export default function App() {
+  const [ showModal, setShowModal ] = useState(false)
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello World
-    </h1>
+    <>
+      <div className='flex flex-row place-content-between'>
+        <div className='flex flex-col ml-14 mt-32'>
+          <h1 className='text-6xl font-bold'>
+            Townhall
+          </h1>
+          <button className='px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm mt-8' onClick={() => setShowModal(true)}>Watch a Tutorial</button>
+          <h3 className='font-bold mt-44'>Whenever you're ready</h3>
+          <button className='text-xl mt-4 px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm' onClick={() => setShowModal(true)}>Start a Meeting Now</button>
+          <button className='text-xl mt-4 px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-md shadow-sm' onClick={() => setShowModal(true)}>Schedule a Meeting Later</button>
+        </div>
+        <div className='flex flex-col text-right mr-14'>
+          <h1 className='mt-8'>Profile</h1>
+          <h1 className='font-bold text-left mt-80'>Planned Meetings</h1>
+          <PlannedMeetings />
+        </div>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+        <GrayedOut show={showModal} />
+      </div>
+    </>
+  )
+}
+
+function Modal({ showModal, setShowModal }) {
+  if (!showModal) return null;
+  return (
+    <div class="z-20 ml-[40%] mt-32 absolute bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all">
+      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+        <div class="sm:flex sm:items-start">
+          <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+            <h3 class="text-lg leading-6 font-medium text-gray-900" id="modal-title">Join the waitlist</h3>
+            <div class="mt-2">
+              <p class="text-sm text-gray-500">Townhall is coming soon -- join the waitlist!</p>
+            </div>
+          </div>
+        </div>
+        <input className='sm:ml-4 border' type='text' placeholder='Email: '/>
+      </div>
+      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+        <button type="button" onClick={() => setShowModal(false)} class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">Submit</button>
+        <button type="button" onClick={() => setShowModal(false)}class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+      </div>
+    </div>
+  )
+}
+
+function PlannedMeetings() {
+  return (
+    <>
+      <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 border">
+        <div class="flex flex-row">
+          <div className='rounded-full bg-gray-200 w-10 h-10 align-self-center mr-4' />
+          <div className='flex flex-col w-5/6'>
+            <div className='w-4/5 h-5 bg-gray-200' />
+            <div className='w-3/5 h-5 bg-gray-200 mt-2'/>
+          </div>
+        </div>
+      </div>
+      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row border">
+        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Invite More Guests</button>
+        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Edit</button>
+        <button type="button" class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">Cancel</button>
+      </div>
+    </>
+  )
+}
+
+function GrayedOut({ show }) {
+  if (!show) return null;
+  return (
+    <div className='z-10 bg-black absolute h-screen w-screen opacity-20' />
   )
 }
